68725299e1278f59be3daecaec26377bd29ceae1
Heather Etheridge <heathernetheridge@gmail.com>
2019-10-05 18:02:10 -0500

    Can now print ship comonents from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { getAllStarships } from './services/sw-api';
+import Starship from './Components/Starship/Starship';
+import Header from './Components/Header/Header';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   
-  render(
-    return(
+  state = {
+    starships: []
+  };
 
-    )
-  )
+  getStarship = (idx) => {
+    return this.state.starships[idx];
+  }
+
+  async componentDidMount() {
+    const starships = await getAllStarships();
+    this.setState({ starships: starships.results });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path='/' render={() => 
+            <Starship 
+              starships={this.state.starships}
+            />
+          }/>
+          {/* <Route path='/starships/:idx' render={(props) => 
+            <StarshipPage
+              {...props}
+              getStarship={this.getStarship}
+            />
+          }/> */}
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
 export default App;
--- src/Components/Starship/Starship.jsx
+++ src/Components/Starship/Starship.jsx
@@ -1,1 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Starship.module.css';
+
+const Starship = (props) => {
+    return(
+        <div className={styles.starships}>
+        {
+            props.starships.map((starship, idx) => {
+                return(
+                    <Link
+                        className={styles.cards}
+                        to={`/starships/${idx}`}
+                        key={starship.name}
+                    >
+                        {starship.name}
+                    </Link>
+                )
+            })
+        }
+        </div>
+    )
+}
+
+export default Starship; 
+
